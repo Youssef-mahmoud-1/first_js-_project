99c4be0a636c862fe53c574deeace5049fa7eca5
Youssef-mahmoud-1 <y.mahmoud292006@gmail.com>
2025-09-15 22:22:40 +0300

    Add files via upload

---
+++ js/min.js
@@ -0,0 +1,9 @@
+// css senctive 
+// interpreter 
+// cammal case 
+
+// document.getElementById('demo').innerHTML  = "Hello From js code"; 
+// document.getElementById('demo').style.color="red"; 
+// document.getElementById('demo').style.background="black"; 
+
+
